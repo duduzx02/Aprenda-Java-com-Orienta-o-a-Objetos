c24b4dfd0fa948645f488ee4915fed6b7c40b6a1
duduzx02 <duduzx02@gmail.com>
2023-04-10 20:53:54 -0300

    Leitura de dados com Scanner

---
+++ PrimieroPrograma/src/Leitura.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.print("Digite seu filme favorito: ");
+        String filme = leitura.nextLine();
+
+        System.out.print("Digite o ano de lançamento: ");
+        int anoDeLancamento = leitura.nextInt();
+
+        System.out.print("Qual sua avalição do filme? de 0 a 10: ");
+        double notaDoFilme = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(notaDoFilme);
+    }
+}
