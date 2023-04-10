b515827b3a4ad32f364cef2c21cac29795f5baf2
duduzx02 <duduzx02@gmail.com>
2023-04-10 04:38:31 -0300

    Trabalhando com textos

--- PrimieroPrograma/src/Main.java
+++ PrimieroPrograma/src/Main.java
@@ -7,5 +7,16 @@ public class Main {
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         boolean includoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        // Média calculada pelas 3 notas do Eduardo, Augusto e Lívia
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento    
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
     }
 }
